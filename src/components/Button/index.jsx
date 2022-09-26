105ce4c2b01c46ae3adae85d1fee6ef4fb527696
Wanderson-Henrique-Oliveira <wandholiveira1@gmail.com>
2022-09-26 18:51:48 -0300

    Alterei o button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ export default function Button() {
 
     return (
         <>
-            <button onClick={() => { getDogs() }}>Adote um doguinho!</button>
+            <button onClick={() => { getDogs() }}>Adote um doguinho</button>
         </>
     )
 }
