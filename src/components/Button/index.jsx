579a1f218a11a68b89b1741d4cd795ee2527033d
Wanderson-Henrique-Oliveira <wandholiveira1@gmail.com>
2022-09-26 18:44:58 -0300

    Add component button

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Button() {
+
+    return (
+        <>
+            <button onClick={() => { getDogs() }}>Adote um doguinho!</button>
+        </>
+    )
+}
