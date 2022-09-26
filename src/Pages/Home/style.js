695a897dcb60cd0bfd2d92be24bca93541dd1d4c
Rafael-Arruda <rafaelsilvaarruda72@gmail.com>
2022-09-26 18:36:23 -0300

    Buscando dados da api

---
+++ src/Pages/Home/style.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+
+    img {   
+        width: 400px;
+        height: 400px;
+        object-fit: cover;
+    }
+`;
